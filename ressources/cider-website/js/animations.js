efade97bd9a3866738b2d72b16d0503cb66a5aa8
Nicolas Petton <nicolas@petton.fr>
2019-09-30 23:46:28 +0200

    Initial commit

---
+++ ressources/cider-website/js/animations.js
@@ -0,0 +1,56 @@
+function animateRepl() {
+	var content = $('.repl .animate .content');
+	var result = $('.repl .result');
+	setTimeout(function() {
+		insertContent(content, function() {
+			insertResult(result, restartAnimation);
+		});
+	}, 4000);
+}
+
+function insertContent(span, callback) {
+	var text = '(println "Cider rocks!")'.split('').reverse();
+	insertTextContent(text, span, callback);
+}
+
+function insertTextContent(text, span, callback, inString) {
+	if (text.length) {
+		var nextChar = text.pop();
+		setTimeout(function() {
+			if(nextChar === ' ') {
+				nextChar = '&nbsp;';
+			} else if(nextChar === '(' || nextChar === ')') {
+				nextChar = '<span class="paren">' + nextChar + '</span>';
+			} else if(nextChar === '"') {
+				inString = !inString;
+			}
+			if (inString || nextChar === '"') {
+				nextChar = '<span class="string">' + nextChar + '</span>';
+			}
+			$('.caret').removeClass('animated');
+			span.html(span.html() + nextChar);
+			insertTextContent(text, span, callback, inString);
+		}, 100);
+	} else {
+		setTimeout(callback, 500);
+	}
+}
+
+function insertResult(span, callback) {
+	$('.caret').addClass('animated');
+	span.text('"Cider rocks!"');
+	$('.first .caret').hide();
+	$('.second').show();
+	setTimeout(restartAnimation, 5000);
+}
+
+function restartAnimation() {
+	$('.first .caret').show();
+	$('.second').hide();
+	$('.repl .animate .content').empty();
+	$('.repl .result').empty();
+	$('.repl').removeClass('show-result');
+	animateRepl();
+}
+
+$(animateRepl);
